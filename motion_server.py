e22b9f818da62dc46a1b65b2e3bfaf3ba6754475
helgestein <helge.stein@rub.de>
2019-11-04 16:24:54 -0800

    added fastapi functionality
    implementing the fastpi calls but still need proper docstrings and there is a need to figure out proper post and get this is however a minimally working example

--- motion_server.py
+++ motion_server.py
@@ -3,7 +3,10 @@
 from fastapi import FastAPI
 import gclib
 import numpy as np
-
+from pydantic import BaseModel
+from enum import Enum
+import uvicorn
+   
 setupd = {'count_to_mm':154.1133/985482.0,
           'galil_ip_str' : '192.168.200.23',
           'def_speed_count_sec':20000,
@@ -13,18 +16,31 @@ setupd = {'count_to_mm':154.1133/985482.0,
           'axlett':'ABCD'}
 
 
-if __main__:
-    #if this is the main instance let us make a galil connection
-    g = gclib.py()
-    print('gclib version:', g.GVersion())
-    g.GOpen('%s --direct -s ALL' %(setupd['galil_ip_str']))
-    print(g.GInfo())
-    c = g.GCommand #alias the command callable
-    app = FastAPI()
 
+#if this is the main instance let us make a galil connection
+g = gclib.py()
+print('gclib version:', g.GVersion())
+g.GOpen('%s --direct -s ALL' %(setupd['galil_ip_str']))
+print(g.GInfo())
+c = g.GCommand #alias the command callable
+app = FastAPI()
 
-@app.put("/motor/move/{x}")
-def motor_move(x_mm: float,axis: str,speed=None,mode='relative'):
+class move_modes(str, Enum):
+    homing = "homing"
+    relative = "relative"
+    absolute = "absolute"
+
+class move_post(BaseModel):
+    x_mm: float
+    axis: str
+    speed: int = None
+    mode: move_modes = 'relative'
+
+@app.get("/motor/")
+def motor_move(x_mm: float,
+               axis: str,
+               speed: int = None,
+               mode: move_modes = 'relative'):
     #this function moves the motor by a set amount of milimeters
     #you have to specify the axis, 
     #if no axis is specified this function throws an error
@@ -37,6 +53,9 @@ def motor_move(x_mm: float,axis: str,speed=None,mode='relative'):
     #motor_move(5,'x',mode='absolute')
     #home the motor at low speed (the distance is not used)
     #motor_move(5,'x',mode='homing',speed=10000)
+    #the server call would look like:
+    #http://127.0.0.1:8000/motor/?x_mm=2&axis=x&mode=absolute
+    #http://127.0.0.1:8000/motor/?x_mm=2&axis=x&mode=relative
     
     #first we check if we have the right axis specified
     if axis in setupd['axis_id'].keys():
@@ -49,6 +68,14 @@ def motor_move(x_mm: float,axis: str,speed=None,mode='relative'):
                 'err_dist':None,
                 'err_code':'setup'}
     
+    #check if the motors are moving if so return an error message
+    if query_moving()['motor_status'] == 'moving':
+        return {'moved_axis':None,
+                'speed':None,
+                'accepted_rel_dist':None,
+                'supplied_rel_dist':x_mm,
+                'err_dist':None,
+                'err_code':'moving'}
     #recalculate the distance in mm into distance in counts
     try:
         float_counts = x_mm/setupd['count_to_mm']#calculate float dist from steupd
@@ -116,7 +143,8 @@ def motor_move(x_mm: float,axis: str,speed=None,mode='relative'):
                 'err_dist':None,
                 'err_code':'motor'}
 
-def disconnect():
+@app.get("/motor/disconnect")
+def motor_disconnect():
     try:
       None
     except gclib.GclibError as e:
@@ -125,11 +153,12 @@ def disconnect():
       g.GClose() #don't forget to close connections!
     return {'connection': 'motor_offline'}
 
-
+@app.get("/motor/query/positions")
 def query_all_axis_positions():
     #this queries all axis positions
     #example: query_all_axis_positions()
-    
+    #a server call should look like 
+    #http://127.0.0.1:8000/motor/query/positions
     #first query the actual position
     ret = c('TP') # query position of all axis
     #now we need to map these outputs to the ABCDEFG... channels
@@ -141,20 +170,26 @@ def query_all_axis_positions():
     #return the results through calculating things into mm
     return {ax_abc_to_xyz[k]:p*setupd['count_to_mm'] for k,p in pos.items()}
 
-
-def query_axis(ax):
+@app.get("/motor/query/position")
+def query_axis(axis):
+    #this only queries the position of a single axis
+    #server example:
+    #http://127.0.0.1:8000/motor/query/position?axis=x
     q = query_all_axis_positions()
-    if ax in q.keys():
-        return {'ax':ax,'position':q[ax]}
+    if axis in q.keys():
+        return {'ax':axis,'position':q[axis]}
     else:
         return {'ax':None,'position':None}
     
-    
+@app.get("/motor/query/moving")    
 def query_moving():
     #this function queries the galil motor controller if any of 
     #it's motors are moving if so it returns moving as a 
     #motor_status otherwise stopped. Stopping codes can mean different things
     #here we just want to know if is is moving or not
+
+    #a server call would look like: 
+    #http://127.0.0.1:8000/motor/query/position?axis=x
     ret = c('SC')
     inv_axis_id = {d:v for v,d in setupd['axis_id'].items()}
     ax_abc_to_xyz = {l:inv_axis_id[l] for i,l in enumerate(setupd['axlett'])}
@@ -163,11 +198,15 @@ def query_moving():
             return {'motor_status':'moving'}
     return {'motor_status':'stopped'}
 
+@app.get("/motor/off")
 def motor_off(axis):
         #sometimes it is useful to turn the motors off for manual alignment
         #this function does exactly that
         #It then returns the status
         #and the current position of all motors
+
+        #an example would be:
+        # http://127.0.0.1:8000/motor/stop
         
         if axis in setupd['axis_id'].keys():
             ax = setupd['axis_id'][axis]
@@ -183,11 +222,14 @@ def motor_off(axis):
         ret.update(query_all_axis_positions())
         return ret
 
+@app.get("/motor/on")
 def motor_on(axis):
         #sometimes it is useful to turn the motors back on for manual alignment
         #this function does exactly that
         #It then returns the status
         #and the current position of all motors
+        # server example
+        #http://127.0.0.1:8000/motor/on?axis=x
         if axis in setupd['axis_id'].keys():
             ax = setupd['axis_id'][axis]
         else:
@@ -201,13 +243,28 @@ def motor_on(axis):
         ret.update(query_all_axis_positions())
         return ret
 
+@app.get("/motor/stop")
 def motor_stop():
         #this immediately stopps all motions turns off themotors for a short
         #time and then turns them back on. It then returns the status
         #and the current position of all motors
+        #a server example would be
+        #http://127.0.0.1:8000/motor/off?axis=x
         cmd_seq = ['AB','MO','SH']
         for cmd in cmd_seq:
             _ = c(cmd)
         ret = query_moving()
         ret.update(query_all_axis_positions())
         return ret
+    
+@app.on_event("shutdown")
+def shutdown_event():
+    #this gets called when the server is shut down or reloaded to ensure a clean
+    #disconnect ... just restart or terminate the server
+    motor_stop()
+    g.GClose()
+    return {'shutdown'}
+
+if __name__ == "__main__":
+    # makes this runnable and debuggable in VScode
+    uvicorn.run(app, host="0.0.0.0", port=8000)
