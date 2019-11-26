b287a84a92f1344ddd9ecd6e18a8be9874f3a055
helgestein <helge.stein@rub.de>
2019-11-26 07:58:28 -0800

    Update elchem_server.py

--- elchem_server.py
+++ elchem_server.py
@@ -4,7 +4,8 @@ from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
 from gamry_driver import *
-
+from fastapi import Query
+from typing import List
 app = FastAPI()
 
 class return_class(BaseModel):
@@ -26,9 +27,19 @@ async def pot_potential_ramp_wrap(Vinit: float, Vfinal: float, Vapex1: float, Va
     return return_class(**poti.potential_cycle(Vinit, Vfinal, Vapex1, Vapex2, ScanInit, ScanApex, ScanFinal, HoldTime0,
                                             HoldTime1, HoldTime2, Cycles, SampleRate, control_mode))
 
+@app.get('/potentiostat/get/eis')
+async def eis_(start_freq:float,end_freq:float,points: int, pot_offset: float = 0):
+    return return_class(**poti.eis(start_freq,end_freq,points,pot_offset))
+
 @app.get("/potentiostat/get/status")
 def status_wrapper():
     return return_class(measurement_type='status_query',parameters={'query':'potentiostat'},data=[poti.status()])
+
+@app.get('/potentiostat/get/signal_arr')
+async def signal_array_(Cycles: int, SampleRate: float, arr: str):
+    arr = [float(i) for i in arr.split(',')]
+    return return_class(**poti.signal_array(Cycles, SampleRate, arr))
+
 
 @app.on_event("shutdown")
 def shutdown_event():
@@ -37,6 +48,7 @@ def shutdown_event():
     poti.close_connection()
     loop.close()
     return {'shutdown'}
+
 
 
 if __name__ == "__main__":
